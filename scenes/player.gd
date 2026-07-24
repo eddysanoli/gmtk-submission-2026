26e4760f55558995a9d701ef90de7f79f289d1eb
Alejandro <windevoxhelb@gmail.com>
2026-07-23 19:42:22 -0600

    Fire cooldown and pause menu
    Implemented a cooldown for the shooting action and started working on the "charging" of the toaster as well as implemented the pause menu that currently cannot be exited out of

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,14 +1,16 @@
 extends CharacterBody3D
 
 @onready var spawn_location = $Position3D
+@onready var cooldown_timer = $Cooldown
 @onready var bread = preload("res://scenes/bread.tscn")
 
 const SPEED = 5.0
 
+var charging = false
 var can_shoot = true
 var dead = false
 
-func _readu():
+func _ready():
 	pass
 	
 func _input(event):
@@ -18,14 +20,13 @@ func _input(event):
 		rotation_degrees.y -= event.relative.x * 0.5
 	
 func _process(_delta):
-	
 	if dead:
 		return
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
+	if Input.is_action_just_pressed("Pause"):
+		pause()
 	
-	
-
 func _physics_process(_delta: float) -> void:
 	if dead:
 		return
@@ -37,15 +38,24 @@ func _physics_process(_delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
 	
 func shoot():
-	var new_bread = bread.instance()
+	if !can_shoot:
+		return
+	var new_bread = bread.instantiate()
 	get_node("../Enviorment").add_child(new_bread)
 	new_bread.global_transform = spawn_location.global_transform
-#	Play Shoot animation
-#	Shoot bread
-#	Make can_shoot false
-#	Timer
-#	Return can_shoot true
+	can_shoot = false
+	cooldown_timer.start()
+
+func pause():
+	get_tree().paused = true
+	$CanvasLayer/PauseScreen.show()
+
+func _on_cooldown_timeout():
+	can_shoot = true
+
+func _on_return_button_up():
+	$CanvasLayer/PauseScreen.hide()
+	get_tree().paused = false
