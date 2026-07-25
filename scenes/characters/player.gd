3e04746309a40fdc77c0d05ce74a48f675940807
Dredrin <feliperamirez003@gmail.com>
2026-07-25 12:56:03 -0600

    Loading updated for all scenarios changes

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -18,6 +18,7 @@ var dead = false
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
+	
 func _input(event):
 	if dead:
 		return
@@ -85,7 +86,9 @@ func _on_return_button_up():
 	Engine.time_scale = 1
 	
 func _on_quit_button_up():
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	Engine.time_scale = 1
+	$CanvasLayer/PauseScreen.hide()
+	game_manager.load_scene_with_loading_screen("res://scenes/menu.tscn")
 
 # ────────────────────DEATH─────────────────────────
 func kill():
@@ -94,7 +97,12 @@ func kill():
 	$CanvasLayer/DeathScreen.show()
 
 func _on_restart_button_up():
-	get_tree().change_scene_to_file("res://scenes/enviorment/stage.tscn")
+	$CanvasLayer.hide()
+	Engine.time_scale = 1
+	game_manager.load_scene_with_loading_screen("res://scenes/enviorment/stage.tscn")
 
 func _on_quit_game_button_up() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+	$CanvasLayer.hide()
+	Engine.time_scale = 1
+	game_manager.load_scene_with_loading_screen("res://scenes/menu.tscn")
+	
